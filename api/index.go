8513b13ee35c566dd13f00decfa2b3479446809a
Anthony Lee <alee792@gmail.com>
2018-12-21 10:14:49 -0600

    Testing out zeit

---
+++ api/index.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// Time gives the current time.
+func Time(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, time.Now().Format(time.RFC1123Z))
+}
