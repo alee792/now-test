f28d9c68e5c30b6293d166c432c78208689a988e
Anthony Lee <alee792@gmail.com>
2018-12-21 11:15:21 -0600

    Commit before rename

--- api/index.go
+++ api/index.go
@@ -6,6 +6,10 @@ import (
 	"time"
 )
 
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s", "wonder what happens now")
+}
+
 // Time gives the current time.
 func Time(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, time.Now().Format(time.RFC1123Z))
